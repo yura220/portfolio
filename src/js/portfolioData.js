5805f82256550fc98e614848e80b3a6b9c65de23
yura220 <ruddbfk7@gmail.com>
2025-07-29 14:51:28 +0900

    해커톤 추가

--- src/js/portfolioData.js
+++ src/js/portfolioData.js
@@ -12,6 +12,16 @@ export default [
   },
   {
     id: 2,
+    title: '해커톤',
+    visitUrl: 'hackathon-gules-nine.vercel.app',
+    thumbnail: '/img/pf2.jpg',
+    imageTop: '/img/pf2-1.jpg',
+    imageBottom: '/img/pf2-2.jpg',
+    tool: 'React, React Router, CSS, Figma',
+    explanation: '해커톤에 디자이너로 참가하여 팀의 UX/UI를 총괄했고, 기획부터 디자인 시스템 구축, 프로토타입 제작까지 주도했습니다.\n디자인 구현에 필요한 인터랙션 가이드를 프론트엔드와 협업하며 구체화했으며, 결과적으로 대상을 수상하는 성과를 이끌어냈습니다.'
+  },
+  {
+    id: 3,
     title: 'LALA 쇼핑몰',
     visitUrl: 'https://vue-rara.vercel.app',
     thumbnail: '/img/pf2.png',
@@ -21,7 +31,7 @@ export default [
     explanation: '팀장으로서 일정과 역할 분담을 총괄하고, Vue 기반의 MPA 라우팅 및 퍼블리싱을 주도했습니다.\n날씨 선택형 카테고리에 따라 상품을 보여주는 구조를 개발했으며, Splide 슬라이더 구현과 반응형 레이아웃 설계,\n크롬/파이어폭스 기반 테스트, 최종 디버깅까지 맡았습니다.'
   },
   {
-    id: 3,
+    id: 4,
     title: '1:D day 클래스',
     visitUrl: 'https://1dday.vercel.app',
     thumbnail: '/img/pf3.png',
@@ -31,7 +41,7 @@ export default [
     explanation: '검색 및 필터 기능이 포함된 1:D day 클래스 사이트 팀 프로젝트입니다.\n전체 디자인, 반응형 구현, 메인페이지와 상세 페이지 퍼블리싱을 담당했습니다.'
   },
   {
-    id: 4,
+    id: 5,
     title: '세미네 부엌',
     visitUrl: 'https://semi-nine.vercel.app',
     thumbnail: '/img/pf4.png',
@@ -41,7 +51,7 @@ export default [
     explanation: '기존 사이트 "세미네 부엌"을 리뉴얼한 랜딩페이지입니다.\n다크모드를 구현하고, 슬릭(Slick)을 활용한 슬라이드 기능도 적용했습니다.'
   },
   {
-    id: 5,
+    id: 6,
     title: '디자인플러스',
     visitUrl: 'https://portfolio2-blond-iota.vercel.app/',
     thumbnail: '/img/pf5.png',
@@ -51,7 +61,7 @@ export default [
     explanation: '디자인플러스 사이트를 클론 코딩한 프로젝트입니다.\nMasonry Grid를 적용해 갤러리의 불필요한 여백을 개선했으며, 하단 슬라이드는 키보드 입력으로도 전환되도록 구현해 사용자 경험을 높였습니다.'
   },
   {
-    id: 6,
+    id: 7,
     title: 'LS그룹',
     visitUrl: 'https://pf2-one.vercel.app/',
     thumbnail: '/img/pf6.png',
@@ -61,7 +71,7 @@ export default [
     explanation: 'LS그룹 공식 홈페이지를 클론 코딩한 프로젝트입니다.\n정적인 구조 기반으로 주요 섹션과 UI 요소들을 충실히 재현하며, 레이아웃 구성에 집중했습니다.'
   },
   {
-    id: 7,
+    id: 8,
     title: 'HB페이퍼',
     visitUrl: 'https://pf3-sand.vercel.app',
     thumbnail: '/img/pf7.png',
@@ -71,9 +81,9 @@ export default [
     explanation: 'HB페이퍼 공식 홈페이지를 클론 코딩한 프로젝트입니다.\njQuery와 slick 라이브러리를 활용해 메인 배너뿐 아니라 다양한 섹션에서 슬라이더 효과를 구현했으며,\n정적인 구조 기반으로 전체 UI와 스타일을 충실히 재현했습니다.\n또한 언어 선택 기능을 추가하여, 한글과 영어 간의 전환이 가능하도록 구성했습니다.'
   },
   {
-    id: 8,
+    id: 9,
     title: 'todo리스트',
-    visitUrl: 'https://vuetodolist-blue.vercel.app/',
+    visitUrl: 'vuetodolist-blue.vercel.app',
     thumbnail: '/img/pf8.png',
     imageTop: '/img/pf8-1.png',
     imageBottom: '/img/pf8-2.png',
@@ -81,7 +91,7 @@ export default [
     explanation: 'Vue 3 기반으로 제작한 랜딩페이지형 투두리스트 프로젝트입니다. 체크 기능과 삭제 기능을 구현했으며, LocalStorage를 연동해 상태 유지가 가능하도록 구성했습니다.'
   },
   {
-    id: 9,
+    id: 10,
     title: '미니블로그',
     visitUrl: 'https://mini-blog-three-sigma.vercel.app',
     thumbnail: '/img/pf9.png',
@@ -91,7 +101,7 @@ export default [
     explanation: 'Vue 3 기반 미니 블로그 프로젝트입니다.\n글 작성, 수정, 삭제 기능을 구현하고 LocalStorage를 활용해\n데이터 저장이 가능하도록 구성했으며, 모바일 화면에 최적화된 반응형 구조로 제작했습니다.'
   },
   {
-    id: 10,
+    id: 11,
     title: '비컴비',
     thumbnail: '/img/pf10.jpg',
     imageTop: '/img/pf10-1.jpg',
@@ -100,18 +110,8 @@ export default [
     explanation: '비컴비 브랜드의 브로슈어 및 웹디자인을 기획·제작하고, 외주 의뢰를 받아 다양한 업계의 카드 디자인을 진행했습니다.\nPhotoshop과 Illustrator를 활용해 인쇄물과 디지털 콘텐츠를 완성했습니다.'
   },
   {
-    id: 11,
-    title: '수철F&B',
-    thumbnail: '/img/pf1.png',
-    imageTop: '/img/pf1.png',
-    imageBottom: '/img/pf1.png',
-    tool: 'Vue 3, Pinia, Splide, SCSS',
-    explanation: 'Vue 기반 쇼핑몰 구조 구현. 날씨 선택형 상품 추천 기능, Splide 슬라이더 구현 및 반응형 레이아웃 설계.'
-  },
-  {
     id: 12,
-    title: 'React 랜딩페이지',
-    visitUrl: 'https://vue-shop.vercel.app',
+    title: '수철F&B',
     thumbnail: '/img/pf1.png',
     imageTop: '/img/pf1.png',
     imageBottom: '/img/pf1.png',
